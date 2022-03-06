75518095e6a964f1c69dad10e50aead1633935ac
miygw <darkmiygw@gmail.com>
2022-03-06 15:59:39 +0900

    feat: GitHub API 通信

---
+++ src/lib/github/api.ts
@@ -0,0 +1,23 @@
+import { Octokit } from '@octokit/rest';
+
+// TODO: アクセストークンはビルド環境（ローカル/vercel）のものを呼び出すようにする
+const octoKit = new Octokit({
+  auth: process.env.GITHUB_TOKEN,
+});
+
+/**
+ * GitHub から 指定されたリポジトリのディレクトリのファイル情報の一覧を取得する。
+ */
+export const getFileMetaInfos = async (
+  owner: string,
+  repo: string,
+  dirPath: string
+) => {
+  const response = await octoKit.rest.repos.getContent({
+    owner,
+    repo,
+    path: dirPath,
+  });
+
+  return response.data;
+};
