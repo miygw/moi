4d5614433b296a2e4db2468688816423f2677dcc
miygw <darkmiygw@gmail.com>
2022-03-13 22:17:27 +0900

    design

--- src/components/writing/MetaDataBlock.tsx
+++ src/components/writing/MetaDataBlock.tsx
@@ -7,7 +7,7 @@ type Props = {
 const MetaDataBlock = ({ metaData }: Props) => {
   return (
     <div className='text-right py-4  border-t border-gray-400'>
-      <ul>
+      <ul className='xl:mr-8'>
         <li className='ml-4'>{`title: ${metaData.title}`}</li>
         <li className='ml-4'>{`date: ${metaData.date}`}</li>
         <li className='ml-4'>{`summary: ${metaData.summary}`}</li>
