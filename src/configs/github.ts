9f186eda83a6423aedda08b51a5cf907b077b4c9
miygw <darkmygw@gmail.com>
2022-03-27 01:19:44 +0900

    refactor: GitHub API 関連

---
+++ src/configs/github.ts
@@ -0,0 +1,10 @@
+import path from "path";
+
+export const github = {
+  api: {
+    ownerName:  'miygw',
+    fileName: 'writing.md',
+    repoName: 'moi-assets',
+    dirPath: path.posix.join('assets', 'writing')
+  }
+}
