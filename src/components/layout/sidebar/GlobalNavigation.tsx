4291e40a9b2bd1e7707f8963ae109069b1b91f70
miygw <darkmygw@gmail.com>
2022-03-26 23:43:23 +0900

    design

--- src/components/layout/sidebar/GlobalNavigation.tsx
+++ src/components/layout/sidebar/GlobalNavigation.tsx
@@ -23,11 +23,11 @@ export const GlobalNavigationPaddingLeft = 'pl-6';
 
 const GlobalNavigation = () => {
   const ctx = useContext(GlobalNavigationContext);
-  const dynamicDesign = ctx.isOpen ? `visible` : `invisible`;
+  const dynamicVisibility = ctx.isOpen ? `visible` : `invisible`;
 
   return (
     <div
-      className={`bg-slate-600 pt-4 fixed left-0 min-h-full ${dynamicDesign} lg:visible lg:w-40 xl:visible xl:w-60 text-black dark:text-white`}
+      className={`bg-white dark:bg-black pt-4 fixed left-0 min-h-full w-3/4 ${dynamicVisibility} sm:w-1/2 md:w-1/3 lg:visible lg:w-40 xl:visible xl:w-60 text-black dark:text-white`}
     >
       <ul>
         {ItemInfos.map((info) => (
