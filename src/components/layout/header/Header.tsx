e10f4748437fbe0d444d59f5cb73cfb2d7d96878
miygw <darkmygw@gmail.com>
2022-03-26 18:08:27 +0900

    feat: ヘッダー固定

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -3,7 +3,7 @@ import Menu from './Menu';
 
 const Header = () => {
   return (
-    <header className='flex justify-center flex-wrap text-center mt-4 mb-12 mx-5'>
+    <header className='sticky justify-center w-full top-0 py-1 bg-white dark:bg-black flex text-center'>
       <Logo />
       <Menu />
     </header>
