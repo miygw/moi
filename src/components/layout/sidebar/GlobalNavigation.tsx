d3942850cf332c5649a378828ae391e20dd3d9b9
miygw <darkmygw@gmail.com>
2022-03-26 22:51:12 +0900

    feat: グローバルナビゲーションにおいて現在ページを強調

--- src/components/layout/sidebar/GlobalNavigation.tsx
+++ src/components/layout/sidebar/GlobalNavigation.tsx
@@ -1,4 +1,6 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 import ThemeChangeButton from '../../parts/ThemeChangeButton';
 
 // TODO: お前はconfigs行き
@@ -46,9 +48,18 @@ type ItemProps = {
 };
 
 const Item = ({ href, text }: ItemProps) => {
+  const [isSelectedState, setIsSelectedState] = useState(false);
+  const router = useRouter();
+  useEffect(() => {
+    const isSelected = isChildrenOrMatchPathName(href, router.pathname);
+    return setIsSelectedState(isSelected);
+  }, [href, router.pathname]);
+
+  const dynamicDesign = isSelectedState ? `bg-gray-200 dark:bg-zinc-800` : ``;
+
   return (
     <div
-      className={`${DefaultPaddingLeft} h-8 hover:bg-sky-200 dark:hover:bg-sky-900`}
+      className={`${DefaultPaddingLeft} ${dynamicDesign} h-8 hover:bg-sky-200 dark:hover:bg-sky-900`}
     >
       <Link href={href}>
         <a className='h-full'>
@@ -57,6 +68,19 @@ const Item = ({ href, text }: ItemProps) => {
       </Link>
     </div>
   );
+};
+
+/**
+ * 「actualPath が parentPath と一致、または parentPath のサブページである」が成り立つかを判断する。
+ */
+const isChildrenOrMatchPathName = (parentPath: string, actualPath: string) => {
+  // ルートパスは任意のパスと前方一致するので、ルートパス専用の評価を行う。
+  if (parentPath === '/') {
+    if (parentPath === actualPath) return true;
+    else return false;
+  }
+
+  return actualPath.indexOf(parentPath) === 0;
 };
 
 export default GlobalNavigation;
