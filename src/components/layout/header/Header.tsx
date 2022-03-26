3fa343f2c5012a6c8b3672536f0534f414082c37
miygw <darkmygw@gmail.com>
2022-03-26 18:11:05 +0900

    feat: 横幅最大長の制限解除

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -3,7 +3,7 @@ import Menu from './Menu';
 
 const Header = () => {
   return (
-    <header className='sticky justify-center w-full top-0 py-1 bg-white dark:bg-black flex text-center'>
+    <header className='sticky justify-center mx-6 top-0 py-1 bg-white dark:bg-black flex text-center'>
       <Logo />
       <Menu />
     </header>
