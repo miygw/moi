4e62a2301423972c0f37115bda5dd2f4ef3b6649
miygw <darkmygw@gmail.com>
2022-03-27 16:14:37 +0900

    feat: ロゴ画像を.svgに差し替え

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -12,7 +12,7 @@ const Footer = () => {
     <footer className='text-center text-gray-400 my-12'>
       <a className='m-4' href='https://github.com/miygw'>
         <Image
-          src={path.posix.join('/', themeDir, 'github.png')}
+          src={path.posix.join('/', themeDir, 'github.svg')}
           alt='github'
           width={logoSize}
           height={logoSize}
@@ -20,7 +20,7 @@ const Footer = () => {
       </a>
       <a className='m-4' href='https://twitter.com/6emcSYackedM6ar'>
         <Image
-          src={path.posix.join('/', themeDir, 'twitter.png')}
+          src={path.posix.join('/', themeDir, 'Twitter.svg')}
           alt='twitter'
           width={logoSize}
           height={logoSize}
