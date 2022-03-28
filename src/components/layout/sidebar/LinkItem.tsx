eb82e6fb06411cae03b171dbe7d04986516e5b9d
miygw <darkmygw@gmail.com>
2022-03-29 00:25:34 +0900

    refactor

--- src/components/layout/sidebar/LinkItem.tsx
+++ src/components/layout/sidebar/LinkItem.tsx
@@ -4,46 +4,11 @@ import { useContext, useEffect, useState } from 'react';
 import { GlobalNavigationContext } from '../../provider/GlobalNavigationProvider';
 import { GlobalNavigationPaddingLeft } from './GlobalNavigation';
 
-// TODO: お前は別ファイル行き
-type ItemProps = {
-  href: string;
-  text: string;
-};
-
-const LinkItem = ({ href, text }: ItemProps) => {
-  // このメニュー項目が現在URLに対応するなら、メニュー項目の表示を強調する。そのための準備。
-  const [isSelectedState, setIsSelectedState] = useState(false);
-  const router = useRouter();
-  useEffect(() => {
-    // 現在パスの親パスがメニュー項目URLと一致すれば、強調する。
-    const isSelected = isChildrenOrMatchPathName(href, router.pathname);
-    return setIsSelectedState(isSelected);
-  }, [href, router.pathname]);
-
-  // メニュー項目クリックでメニューを閉じる関数の定義
-  const ctx = useContext(GlobalNavigationContext);
-  const closeGlobalNavigation = () => ctx.setIsOpen(false);
-
-  const dynamicDesign = isSelectedState ? `bg-gray-200 dark:bg-zinc-800` : ``;
-
-  return (
-    <div
-      className={`${GlobalNavigationPaddingLeft} ${dynamicDesign} h-8 hover:bg-sky-200 dark:hover:bg-sky-900`}
-    >
-      <Link href={href}>
-        <a className='h-full' onClick={closeGlobalNavigation}>
-          <div className='w-full h-full'>{text}</div>
-        </a>
-      </Link>
-    </div>
-  );
-};
-
 /**
  * 「actualPath が parentPath と一致、または parentPath のサブページである」が成り立つかを判断する。
  */
 const isChildrenOrMatchPathName = (parentPath: string, actualPath: string) => {
-  // ルートパスは任意のパスと前方一致するので、ルートパス専用の評価を行う。
+  // ルートパスは任意のパスと前方一致するので、ルートパスであるかは完全一致で評価する。
   if (parentPath === '/') {
     if (parentPath === actualPath) return true;
     else return false;
@@ -52,4 +17,36 @@ const isChildrenOrMatchPathName = (parentPath: string, actualPath: string) => {
   return actualPath.indexOf(parentPath) === 0;
 };
 
-export default LinkItem;
+type Props = {
+  href: string;
+  text: string;
+};
+
+export default function LinkItem({ href, text }: Props) {
+  // このLinkItemが現在URLに対応するなら表示を強調する。
+  const [isSelectedState, setIsSelectedState] = useState(false);
+  const onSelectedColor = isSelectedState ? `bg-gray-200 dark:bg-zinc-800` : ``;
+
+  // 現在パスの親パスがLinkItemのhrefと一致すれば強調する。
+  const router = useRouter();
+  useEffect(() => {
+    const isSelected = isChildrenOrMatchPathName(href, router.pathname);
+    return setIsSelectedState(isSelected);
+  }, [href, router.pathname]);
+
+  // LinkItemクリック => サイドバーを閉じる処理
+  const globalNavigationContext = useContext(GlobalNavigationContext);
+  const closeGlobalNavigation = () => globalNavigationContext.setIsOpen(false);
+
+  return (
+    <div
+      className={`${GlobalNavigationPaddingLeft} ${onSelectedColor} h-8 hover:bg-sky-200 dark:hover:bg-sky-900`}
+    >
+      <Link href={href}>
+        <a className='h-full' onClick={closeGlobalNavigation}>
+          <div className='w-full h-full'>{text}</div>
+        </a>
+      </Link>
+    </div>
+  );
+}
