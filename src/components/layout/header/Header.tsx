97bcf937b7fce4aff5de03161852978ed10ae670
miygw <darkmygw@gmail.com>
2022-03-28 23:04:41 +0900

    design

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -3,7 +3,7 @@ import Logo from './Logo';
 
 const Header = () => {
   return (
-    <header className='sticky flex top-0 bg-white dark:bg-zinc-900 text-center'>
+    <header className='sticky flex top-0 bg-white dark:bg-black text-center'>
       <Logo />
       <Description />
     </header>
