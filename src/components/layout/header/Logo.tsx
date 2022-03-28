d707ee5d4baacafb2238018e1fb316e67032c793
miygw <darkmygw@gmail.com>
2022-03-28 22:44:47 +0900

    fix: デスクトップサイズでもロゴクリック時のオーバーレイが表示される

--- src/components/layout/header/Logo.tsx
+++ src/components/layout/header/Logo.tsx
@@ -1,15 +1,17 @@
 import Image from 'next/image';
 import { useContext } from 'react';
+import { useMediaQuery } from 'react-responsive';
 import { GlobalNavigationContext } from '../../provider/GlobalNavigationProvider';
 
 const Logo = () => {
   const ctx = useContext(GlobalNavigationContext);
   const setIsOpen = () => ctx.setIsOpen(!ctx.isOpen);
+  const isLg = useMediaQuery({query: '(min-width: 1024px)'});
 
   return (
-    <a onClick={setIsOpen}>
+    <div onClick={isLg ? () => {} : setIsOpen} className='m-2 min-w-fit'>
       <Image src={'/miygw.jpg'} alt='logo' width={50} height={50} />
-    </a>
+    </div>
   );
 };
 
