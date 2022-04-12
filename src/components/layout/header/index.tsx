05f0c51592a8e6f2b1d2fbbcf0d78f7af7a111cc
miygw <darkmygw@gmail.com>
2022-04-12 23:23:07 +0900

    fix: <header> の z-index が指定されていない

--- src/components/layout/header/index.tsx
+++ src/components/layout/header/index.tsx
@@ -1,9 +1,12 @@
 import Description from './Description';
 import Logo from './Logo';
+import { layout } from '../../../configs/layout';
 
 export default function Header() {
   return (
-    <header className='sticky flex top-0 bg-white dark:bg-black text-center'>
+    <header
+      className={`${layout.zAxis.header} sticky flex top-0 bg-white dark:bg-black text-center`}
+    >
       <Logo />
       <Description />
     </header>
--- src/configs/layout.ts
+++ src/configs/layout.ts
@@ -1,6 +1,7 @@
 export const layout = {
   zAxis: {
     globalNavigation: 'z-20',
+    header: 'z-20',
     overlay: 'z-10',
   },
 };
