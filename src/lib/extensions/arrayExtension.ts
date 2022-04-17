22d4ee7569c4dff89c26404ffd4f00b4c1c5974a
miygw <darkmygw@gmail.com>
2022-04-17 17:21:29 +0900

    feat: 配列シャッフル機能

---
+++ src/lib/extensions/arrayExtension.ts
@@ -0,0 +1,23 @@
+/**
+ * （非破壊）与えられた配列をシャッフルした配列を生成する。
+ */
+export const shuffle = <T>(target: T[]) => {
+  // target を破壊しないよう、シャッフル対象は target のディーブコピーとする。
+  const result = target.slice();
+
+  // Durstenfeld shuffle：
+  // 計算量の少ないシャッフルアルゴリズム。配列のインデックスに関して降順に以下の手続きを繰り返し行う。
+  // 現在のインデックスを i とする。
+  // 1. 0 ~ i からランダムな整数 k を1つ選ぶ。
+  // 2. 配列内の i 番目の要素と k 番目の要素を入れ替える。
+  for (var count = result.length - 1; count > 0; count--) {
+    var randomNumber = Math.floor(Math.random() * count);
+
+    [result[count], result[randomNumber]] = [
+      result[randomNumber],
+      result[count],
+    ];
+  }
+
+  return result;
+};
