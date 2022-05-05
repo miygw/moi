14108e0b9069160a6319df6e8e449dd93c4a45eb
miygw <darkmygw@gmail.com>
2022-05-05 22:51:52 +0900

    refactor: Google Analytics周り

---
+++ src/hooks/useGAWithRouter.ts
@@ -0,0 +1,21 @@
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import { typeResolve } from '../lib/ts/type';
+
+export function useGAWithRouter() {
+  const router = useRouter();
+  useEffect(() => {
+    const gaHandler = (url: string) => {
+      const params: Gtag.ConfigParams = { page_path: url };
+      window.gtag(
+        'config',
+        typeResolve<string>(process.env.NEXT_PUBLIC_GA_ID),
+        params
+      );
+    };
+    router.events.on('routeChangeComplete', gaHandler);
+    return () => {
+      router.events.off('routeChangeComplete', gaHandler);
+    };
+  }, [router.events]);
+}
