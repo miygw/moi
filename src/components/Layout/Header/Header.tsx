07e206094c731741d062d57fac6703c1840d5815
miygw <darkmygw@gmail.com>
2022-05-09 00:23:38 +0900

    fix: ヘッダーとサイドバーの背景色が指定されていない

--- src/components/Layout/Header/Header.tsx
+++ src/components/Layout/Header/Header.tsx
@@ -5,7 +5,7 @@ import { PageTitle } from './PageTitle';
 export const Header = () => {
   return (
     <header
-      className={`${layoutConfigs.zIndex.header} sticky top-0 flex text-center`}
+      className={`${layoutConfigs.zIndex.header} sticky top-0 flex bg-white text-center dark:bg-black`}
     >
       <Logo />
       <PageTitle />
