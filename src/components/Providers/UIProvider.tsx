91fab31a347f66647232500767cf43959b1306a4
miygw <darkmygw@gmail.com>
2022-05-23 01:52:17 +0900

    some

--- src/components/Providers/UIProvider.tsx
+++ src/components/Providers/UIProvider.tsx
@@ -1,12 +1,12 @@
 import {
   createContext,
+  Dispatch,
   PropsWithChildren,
   useContext,
   useEffect,
   useReducer,
 } from 'react';
 import { useMediaQuery } from 'react-responsive';
-import { UIController } from './UIController';
 
 export type UIState = {
   isMobileSize: boolean;
@@ -15,7 +15,7 @@ export type UIState = {
   displayOverlay: boolean;
 };
 
-export type UIActionType =
+export type UIActionTypes =
   | { type: 'SET_IS_MOBILE_SIZE'; value: boolean }
   | { type: 'SET_PAGE_TITLE'; value: string }
   | { type: 'OPEN_SIDEBAR' }
@@ -27,6 +27,8 @@ type UIActions = {
   setPageTitle: (value: string) => void;
   openSidebar: () => void;
   closeSidebar: () => void;
+  openOverlay: () => void;
+  closeOverlay: () => void;
 };
 
 // useUIでラップするため、exportしない。
@@ -34,28 +36,42 @@ const UIStateContext = createContext<UIState | null>(null);
 const UIActionsContext = createContext<UIActions | null>(null);
 
 export const UIProvider = ({ children }: PropsWithChildren<{}>) => {
-  const initialState: UIState = {
+  const reducer = createReducer();
+  const initialState = createInitialState();
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const actions: UIActions = createUIActions(dispatch);
+
+  useDisplaySizeWatcher(dispatch);
+  return (
+    <UIStateContext.Provider value={state}>
+      <UIActionsContext.Provider value={actions}>
+        {children}
+      </UIActionsContext.Provider>
+    </UIStateContext.Provider>
+  );
+};
+
+const createInitialState = () => {
+  return {
     isMobileSize: true,
     pageTitle: '',
     displaySidebar: false,
     displayOverlay: false,
-  };
+  } as UIState;
+};
 
-  // 1024pxは、Tailwindcssのデフォルトのlgブレークポイントの値。
-  const isLg = useMediaQuery({ minWidth: 1024 });
-
-  const reducer = (state: UIState, action: UIActionType): UIState => {
-    switch (action.type) {
+const createReducer = () => {
+  return (state: UIState, actionType: UIActionTypes): UIState => {
+    switch (actionType.type) {
       case 'SET_IS_MOBILE_SIZE':
-        return { ...state, isMobileSize: action.value };
+        return { ...state, isMobileSize: actionType.value };
       case 'SET_PAGE_TITLE':
-        return { ...state, pageTitle: action.value };
+        return { ...state, pageTitle: actionType.value };
       case 'OPEN_SIDEBAR':
-        return { ...state, displaySidebar: true, displayOverlay: !isLg };
+        return { ...state, displaySidebar: true };
       case 'CLOSE_SIDEBAR':
-        // デスクトップサイズならサイドバーは常に表示する
-        if (isLg) return state;
-        return { ...state, displaySidebar: false, displayOverlay: false };
+        return { ...state, displaySidebar: false };
       case 'OPEN_OVERLAY':
         return { ...state, displayOverlay: true };
       case 'CLOSE_OVERLAY':
@@ -64,38 +80,47 @@ export const UIProvider = ({ children }: PropsWithChildren<{}>) => {
         return state;
     }
   };
+};
 
-  const [state, dispatch] = useReducer(reducer, initialState);
-
+const createUIActions = (dispatch: Dispatch<UIActionTypes>) => {
   const setPageTitle = (value: string) =>
     dispatch({ type: 'SET_PAGE_TITLE', value });
   const openSidebar = () => dispatch({ type: 'OPEN_SIDEBAR' });
   const closeSidebar = () => dispatch({ type: 'CLOSE_SIDEBAR' });
-  // オーバーレイの制御はこのコンポーネントで行うため配信しない。
-  const actions: UIActions = { setPageTitle, openSidebar, closeSidebar };
+  const openOverlay = () => dispatch({ type: 'OPEN_OVERLAY' });
+  const closeOverlay = () => dispatch({ type: 'CLOSE_OVERLAY' });
 
-  // サイドバー表示中のモバイルサイズからデスクトップサイズに変わった場合、
-  // オーバーレイを非表示にする。
-  useEffect(() => {
-    if (isLg && state.displaySidebar) dispatch({ type: 'CLOSE_OVERLAY' });
-  }, [isLg, state.displaySidebar]);
+  return {
+    setPageTitle,
+    openSidebar,
+    closeSidebar,
+    openOverlay,
+    closeOverlay,
+  } as UIActions;
+};
 
-  // デスクトップサイズからモバイルサイズに変わった場合、
-  // デスクトップサイズでは固定表示のサイドバーを閉じる。
-  useEffect(() => {
-    if (!isLg) closeSidebar();
-  }, [isLg]);
-
-  return (
-    <UIStateContext.Provider value={state}>
-      <UIActionsContext.Provider value={actions}>
-        <UIController state={state} dispatch={dispatch} />
-        {children}
-      </UIActionsContext.Provider>
-    </UIStateContext.Provider>
+const useDisplaySizeWatcher = (dispatch: Dispatch<UIActionTypes>) => {
+  const isMobileSize = !useMediaQuery({ minWidth: 1024 });
+  useEffect(() =>
+    dispatch({ type: 'SET_IS_MOBILE_SIZE', value: isMobileSize })
   );
 };
 
 // For useUI
 export const useUIState = () => useContext(UIStateContext);
 export const useUIActions = () => useContext(UIActionsContext);
+
+// // 1024pxは、Tailwindcssのデフォルトのlgブレークポイントの値。
+// const isLg = useMediaQuery({ minWidth: 1024 });
+
+// // サイドバー表示中のモバイルサイズからデスクトップサイズに変わった場合、
+// // オーバーレイを非表示にする。
+// useEffect(() => {
+//   if (isLg && state.displaySidebar) dispatch({ type: 'CLOSE_OVERLAY' });
+// }, [isLg, state.displaySidebar]);
+
+// // デスクトップサイズからモバイルサイズに変わった場合、
+// // デスクトップサイズでは固定表示のサイドバーを閉じる。
+// useEffect(() => {
+//   if (!isLg) closeSidebar();
+// }, [isLg]);
