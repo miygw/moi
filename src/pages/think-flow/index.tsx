43d2e0a44f526dfaf579de07f61ca2ba10e13eca
miygw <darkmygw@gmail.com>
2022-07-17 01:40:34 +0900

    refactor

--- src/pages/think-flow/index.tsx
+++ src/pages/think-flow/index.tsx
@@ -1,26 +1,25 @@
 import { PrismaClient, think_flow } from '@prisma/client';
-import { GetStaticProps } from 'next';
+import { InferGetStaticPropsType } from 'next';
 import { DynamicHead } from '~/components/Head';
 import ThinkFlow from '~/components/ThinkFlow';
 import { shuffle } from '~/lib/extensions/arrayExtension';
 
-type Props = {
-  thinkFlows: think_flow[];
-};
+type Props = InferGetStaticPropsType<typeof getStaticProps>;
 
-export default function ThinkFlowPage({ thinkFlows }: Props) {
+export default function ThinkFlowPage(props: Props) {
   return (
     <>
       <DynamicHead title='Think Flow' />
-      <ThinkFlow thinkFlows={thinkFlows} />
+      <ThinkFlow thinkFlows={props.shuffledThunkFlows} />
     </>
   );
 }
 
-export const getStaticProps: GetStaticProps<Props> = async () => {
-  const prisma = new PrismaClient();
-  let thinkFlows = await prisma.think_flow.findMany();
-  thinkFlows = JSON.parse(JSON.stringify(thinkFlows));
-  thinkFlows = shuffle<think_flow>(thinkFlows);
-  return { props: { thinkFlows } };
+export const getStaticProps = async () => {
+  const pc = new PrismaClient();
+  const raw = await pc.think_flow.findMany();
+  const parsed = JSON.parse(JSON.stringify(raw));
+  const shuffledThunkFlows = shuffle<think_flow>(parsed);
+
+  return { props: { shuffledThunkFlows } };
 };
